002e721ccdb40a232b6f9283b81f082d1d499ac8
hcconnor <hamiltoncconnor@gmail.com>
2015-07-16 08:33:03 +0000

    added email config to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,16 @@ Rails.application.configure do
 
   # Adding for asset precompile on heroku
   #config.serve_static_assets = true;
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  :address              => "smtp.gmail.com",
+  :port                 => 587,
+  :domain               => "gmail.com",
+  :user_name            => ENV['EMAIL'],
+  :password             => ENV['PASSWORD'],
+  :authentication       => "plain",
+  :enable_starttls_auto => true
+}
 end
