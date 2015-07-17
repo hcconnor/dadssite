0bbe35379279b386136a19f8f4e734da5210b031
hcconnor <hamiltoncconnor@gmail.com>
2015-07-17 00:22:44 +0000

    added code to evironment to allow for env vars

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,7 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
+app_environment_variables = File.join(Rails.root, 'config', 'initializers', 'app_env_vars.rb')
+load(app_environment_variables) if File.exists?(app_environment_variables)
 # Initialize the Rails application.
 Rails.application.initialize!
